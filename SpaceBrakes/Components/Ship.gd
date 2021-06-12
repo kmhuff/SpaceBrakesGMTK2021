e62cc76906f3deabfc2c8e6e1f640b04408446fb
Kurtis Huff <kurtis.m.huff@gmail.com>
2021-06-12 09:29:37 -0500

    Add ship textures

--- SpaceBrakes/Components/Ship.gd
+++ SpaceBrakes/Components/Ship.gd
@@ -3,6 +3,7 @@ extends RigidBody2D
 
 export var initial_angle_deg = 0
 export var initial_speed = 200
+export (Texture) var ship_texture
 
 onready var sprite = $Sprite
 
@@ -10,7 +11,10 @@ onready var sprite = $Sprite
 func _ready():
 	linear_velocity = Vector2(initial_speed * cos(deg2rad(initial_angle_deg)), 
 			initial_speed * sin(deg2rad(initial_angle_deg)))
+	
+	sprite.texture = ship_texture
+	sprite.rotation = linear_velocity.angle() + (PI/2)
 
 
 func _process(_delta):
-	sprite.rotation = linear_velocity.angle()
+	sprite.rotation = linear_velocity.angle() + (PI/2)
