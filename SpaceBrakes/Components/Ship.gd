423d52c07894bc9eaab463aee9728b25f840b38f
Kurtis Huff <kurtis.m.huff@gmail.com>
2021-06-12 10:07:08 -0500

    Add space brakes

--- SpaceBrakes/Components/Ship.gd
+++ SpaceBrakes/Components/Ship.gd
@@ -1,11 +1,15 @@
 extends RigidBody2D
 
 
+const BRAKE_MAG = 10
+
 export var initial_angle_deg = 0
 export var initial_speed = 200
 export (Texture) var ship_texture
+export var brake_input = "green_space_brakes"
 
 onready var sprite = $Sprite
+onready var shield_sprite = $ShieldSprite
 
 
 func _ready():
@@ -18,3 +22,15 @@ func _ready():
 
 func _process(_delta):
 	sprite.rotation = linear_velocity.angle() + (PI/2)
+
+
+func _integrate_forces(state):
+	if Input.is_action_just_pressed(brake_input):
+		shield_sprite.visible = true
+		mass = mass * BRAKE_MAG
+		state.linear_velocity = state.linear_velocity / BRAKE_MAG
+	
+	if Input.is_action_just_released(brake_input):
+		shield_sprite.visible = false
+		mass = mass / BRAKE_MAG
+		state.linear_velocity = state.linear_velocity * BRAKE_MAG
